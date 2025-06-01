f05efd53e75d0adcbc0eacfb3f81e46160643a33
FirstClassTree <phoenix@TheKewlLaptop.localdomain>
2025-06-01 18:30:02 +0300

    added files and in main the neccerry api commented

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include <./common/GameManager.h>
+
 int main(){
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
+// int main(int argc, char** argv) {
+
+// GameManager game(MyPlayerFactory{}, MyTankAlgorithmFactory{});
+// game.readBoard(<game_board_input_file from command line>);
+// 	game.run();
+// }
