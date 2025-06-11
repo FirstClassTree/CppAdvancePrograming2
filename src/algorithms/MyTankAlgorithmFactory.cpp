5ef6e044e5a8c0a0d1ceaa3cc2ddac6d01a16782
FirstClassTree <phoenix@TheKewlLaptop.localdomain>
2025-06-11 19:13:04 +0300

    retry for ci cd

--- src/algorithms/MyTankAlgorithmFactory.cpp
+++ src/algorithms/MyTankAlgorithmFactory.cpp
@@ -7,12 +7,8 @@ MyTankAlgorithmFactory::MyTankAlgorithmFactory() {}
 MyTankAlgorithmFactory::~MyTankAlgorithmFactory() {}
 
 std::unique_ptr<TankAlgorithm> MyTankAlgorithmFactory::create(int player_index, int tank_index) {
-    // every 4 sniper tanks
-<<<<<<< HEAD
-    if((tank_index % 2 ) == 0 ){
-=======
-    if(((tank_index +1) % 4) == 0 ){
->>>>>>> ffe609fa5683a64988b755ff1ce7e4fbf445ee39
+    // switch between algoirthms
+    if(((tank_index +player_index) % 2 )   == 0 ){
         return std::make_unique<ChaserTankAlgorithm>(player_index, tank_index,this->tank_x,this->tank_y);
     } else {
         return std::make_unique<SniperTankAlgorithm>(player_index, tank_index,this->tank_x,this->tank_y);
