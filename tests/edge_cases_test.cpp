b95b7e6f37ea7b4634b58dc4a4088bfd267b23f3
LiorBuch <lior.buchmeier@gmail.com>
2025-06-11 21:18:19 +0300

    feat: removed problematic tests and added edge cases maps

--- tests/edge_cases_test.cpp
+++ tests/edge_cases_test.cpp
@@ -84,3 +84,35 @@ TEST(EdgeCaseMaps, ZeroMap) {
   game.run();
   EXPECT_EQ(true,true);
 }
+
+TEST(EdgeCaseMaps, bigRows) {
+
+  std::filesystem::path current_test_file_path = __FILE__;
+  std::filesystem::path project_root_path =
+      current_test_file_path.parent_path().parent_path();
+  std::filesystem::path map_file_path =
+      project_root_path / "files" / "tests" / "edge_cases" / "test_map_edge_rows.txt";
+  
+
+  GameManager game(GamePlayerFactory{}, MyTankAlgorithmFactory{}, false);
+  auto result = game.load_map(map_file_path.string());
+  EXPECT_EQ(result, 0);
+  game.run();
+  EXPECT_EQ(true,true);
+}
+
+TEST(EdgeCaseMaps, bigCols) {
+
+  std::filesystem::path current_test_file_path = __FILE__;
+  std::filesystem::path project_root_path =
+      current_test_file_path.parent_path().parent_path();
+  std::filesystem::path map_file_path =
+      project_root_path / "files" / "tests" / "edge_cases" / "test_map_edge_cols.txt";
+  
+
+  GameManager game(GamePlayerFactory{}, MyTankAlgorithmFactory{}, false);
+  auto result = game.load_map(map_file_path.string());
+  EXPECT_EQ(result, 0);
+  game.run();
+  EXPECT_EQ(true,true);
+}
